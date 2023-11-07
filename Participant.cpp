c5897ba21688e2527586e8d6cff5f4b304ca6c39
ryouhei tominaga <tomi1444@gmail.com>
2023-11-07 11:38:03 +0900

    勘違いしていたルールの改善

--- Participant.cpp
+++ Participant.cpp
@@ -3,35 +3,39 @@
 
 using namespace std;
 //trun:trueディーラ、false:プレイヤ-,
-int Participant::CardSumSetter(int sum,bool trun) {
+int Participant::CardSumSetter(int sum, bool trun) {
 	_card = sum;
-	
+
 	if (_card > 10) {
 		_card = 10;
 	}
 	if (_card == 1) {
-		if (_cardSum < 11&&trun) {
+		if (_cardSum < 11 && trun) {
 			_card = 11;
 			_dOneCard++;
 		}
-		else if(_cardSum < 11 && trun==false)
+		else if (_cardSum < 11 && trun == false)
 		{
 			_card = 11;
 			_dOneCard++;
 		}
 	}
-	
+
 	_cardSum += _card;
-	if (_cardSum > 21 && _dOneCard> 0) {
-		_cardSum -= 10;
-		_dOneCard = 0;
-		
+	if (trun) {
+		if (_cardSum > 21 && _dOneCard > 0) {
+			_cardSum -= 10;
+			_dOneCard = 0;
+
+		}
 	}
-	else if (_cardSum > 21 && _pOneCard > 0) {
-		_cardSum -= 10;
-		_pOneCard = 0;
-	
+	else if (trun == false) {
+		if (_cardSum > 21 && _pOneCard > 0) {
+			_cardSum -= 10;
+			_pOneCard = 0;
+
+		}
 	}
-	
+
 	return  _cardSum;
 }
